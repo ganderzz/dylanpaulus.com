4f207ef3ba2bbf4ae7d86ceb3dfe17859843cfed
ganderzz <stolen3@gmail.com>
2020-11-01 11:17:22 -0800

    fix: reduce darkmode flicker

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,7 +9,16 @@ export default function About() {
     <Layout>
       <SEO
         title="About"
-        keywords={[`software`, `code`, `programming`, `blog`, `portfolio`, `react`, `typescript`, `javascript`]}
+        keywords={[
+          `software`,
+          `code`,
+          `programming`,
+          `blog`,
+          `portfolio`,
+          `react`,
+          `typescript`,
+          `javascript`,
+        ]}
       />
       <section className="sm:p-8 p-4">
         <div className="md:flex sm:block leading-loose">
@@ -19,15 +28,21 @@ export default function About() {
               Hi, I'm Dylan.
             </em>
             <p className="text-base">
-              A fullstack developer working primarily in React and C#. In the past I was known for my PHP and Scala, but
-              those days are long gone. I'm a fan of graphic design/UX, Postgres, Docker/Kubernetes, and teaching. The
-              site you are currently looking at was designed to{" "}
-              <a href="https://www.hanselman.com/blog/do-they-deserve-the-gift-of-your-keystrokes">save keystrokes</a>.
+              A fullstack developer working primarily in React and C#. In the
+              past I was known for my PHP and Scala, but those days are long
+              gone. I'm a fan of graphic design/UX, Postgres, Docker/Kubernetes,
+              and teaching. The site you are currently looking at was designed
+              to{" "}
+              <a href="https://www.hanselman.com/blog/do-they-deserve-the-gift-of-your-keystrokes">
+                save keystrokes
+              </a>
+              .
               <br />
               Let's learn something new together.
               <br />
               <br />
-              For questions or inqueries: <a href="mailto:dylanpaulus@hey.com">📧</a>
+              For questions or inqueries:{" "}
+              <a href="mailto:dylanpaulus@hey.com">📧</a>
             </p>
             <div className="text-center mt-8">
               <img
@@ -41,7 +56,9 @@ export default function About() {
                 }}
               />
 
-              <figcaption class="gatsby-resp-image-figcaption">That's me, the one in the middle!</figcaption>
+              <figcaption class="gatsby-resp-image-figcaption">
+                That's me, the one in the middle!
+              </figcaption>
             </div>
           </div>
 
@@ -50,44 +67,60 @@ export default function About() {
 
             <Timeline>
               <li class="mb-2">
-                <div class="flex-1 ml-6 font-base text-secondary-400 italic">Now</div>
+                <div class="flex-1 ml-6 font-base text-secondary-400 italic">
+                  Now
+                </div>
               </li>
 
-              <TimelineItem>
+              <TimelineItem isActive>
                 <strong>Lead Software Engineer</strong>
-                <div className="text-gray-600 -mt-2">Schweitzer Engineering Laboratories</div>
+                <div className="text-gray-600 -mt-2">
+                  Schweitzer Engineering Laboratories
+                </div>
               </TimelineItem>
 
               <TimelineItem>
                 <strong>Software Engineer</strong>
-                <div className="text-gray-600 -mt-2">Schweitzer Engineering Laboratories</div>
+                <div className="text-gray-600 -mt-2">
+                  Schweitzer Engineering Laboratories
+                </div>
               </TimelineItem>
 
               <TimelineItem>
                 <strong>Associate Software Engineer</strong>
-                <div className="text-gray-600 -mt-2">Schweitzer Engineering Laboratories</div>
+                <div className="text-gray-600 -mt-2">
+                  Schweitzer Engineering Laboratories
+                </div>
               </TimelineItem>
 
               <TimelineItem>
                 <a href="https://www.mobygames.com/developer/sheet/view/developerId,783203/">
-                  <strong>Web Developer</strong> <div className="text-gray-600 -mt-2">Sony Playstation</div>
+                  <strong>Web Developer</strong>{" "}
+                  <div className="text-gray-600 -mt-2">Sony Playstation</div>
                 </a>
               </TimelineItem>
 
               <TimelineItem>
                 <strong>Student Web Developer</strong>{" "}
-                <div className="text-gray-600 -mt-2">Eastern Washington University</div>
+                <div className="text-gray-600 -mt-2">
+                  Eastern Washington University
+                </div>
               </TimelineItem>
 
               <li>
-                <div class="flex-1 ml-6 font-base text-secondary-400 italic">2013</div>
+                <div class="flex-1 ml-6 font-base text-secondary-400 italic">
+                  2013
+                </div>
               </li>
             </Timeline>
 
             <SubHeading>Talks</SubHeading>
             <ul className="list-disc list-inside">
               <li>
-                <a target="_blank" href="https://introduction-functional-programming-boise.netlify.com/#/">
+                <a
+                  target="_blank"
+                  href="https://introduction-functional-programming-boise.netlify.com/#/"
+                >
                   Functional Programming Light (Boise Code Camp 2019)
                 </a>
               </li>
