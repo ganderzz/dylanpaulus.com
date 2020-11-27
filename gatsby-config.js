94b913e6593157496649f6297cecfb55722a8180
ganderzz <stolen3@gmail.com>
2020-11-27 10:15:44 -0800

    chore: remove unused config dep

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     "gatsby-plugin-svgr",
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-mdx`,
     `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-transformer-remark`,
