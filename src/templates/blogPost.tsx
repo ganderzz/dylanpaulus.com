7ca3146b875cf87fc906b6f0aa6d995973c25393
ganderzz <stolen3@gmail.com>
2022-07-04 12:20:25 -0700

    feat: add tips for new software engineers

--- src/templates/blogPost.tsx
+++ src/templates/blogPost.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
-import { Giscus } from "@giscus/react";
+import Giscus from "@giscus/react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import { IGatsbyQuery } from "../interfaces/IGatsbyQuery";
@@ -40,7 +40,9 @@ export default function BlogPost(payload: Props) {
           className="flex justify-between content-center text-base bg-orange-100 rounded-t-md border-t-4 border-orange-500 text-orange-700 p-4"
           role="alert"
         >
-          <p className="relative top-0 mt-2">⚠️ This post is not published! Content is subject to change.</p>{" "}
+          <p className="relative top-0 mt-2">
+            ⚠️ This post is not published! Content is subject to change.
+          </p>{" "}
           <a
             href={`https://github.com/ganderzz/dylanpaulus.com/blob/master/posts/${parent.name}.${parent.extension}`}
             className="bg-white hover:text-black hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
@@ -56,14 +58,19 @@ export default function BlogPost(payload: Props) {
 
       <section className="mt-4" id="main-content" tabIndex={-1}>
         <h1
-          className={`${frontmatter.series ? "mb-0" : "mb-10"} mt-4 mx-auto font-bold text-center`}
+          className={`${
+            frontmatter.series ? "mb-0" : "mb-10"
+          } mt-4 mx-auto font-bold text-center`}
           style={{ maxWidth: "45ch" }}
         >
           {frontmatter.title}
         </h1>
 
         {frontmatter.series && (
-          <h6 className="mb-10 mt-0 mx-auto italic text-center" style={{ maxWidth: "45ch" }}>
+          <h6
+            className="mb-10 mt-0 mx-auto italic text-center"
+            style={{ maxWidth: "45ch" }}
+          >
             Part of the {frontmatter.series} series.
           </h6>
         )}
