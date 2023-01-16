da732461d01856a7b3c80796ff2f6922df35c763
Dylan Paulus <dylan.paulus@rover.com>
2023-01-15 21:25:20 -0800

    add pagination

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,19 +7,21 @@ import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [compress(), image(), preact(), tailwind(), sitemap()],
-    output: "static",
-    site: `https://dylanpaulus.com`,
-    vite: {
-        ssr: {
-            external: ["svgo"],
-        },
-    },
-    markdown: {
-        extendDefaultPlugins: true,
-        syntaxHighlight: "shiki",
-        remarkPlugins: [remarkReadingTime, remarkExcerpt],
-    },
+  integrations: [compress(), image(), preact(), tailwind(), sitemap(), mdx()],
+  output: "static",
+  site: `https://dylanpaulus.com`,
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  },
+  markdown: {
+    extendDefaultPlugins: true,
+    syntaxHighlight: "shiki",
+    remarkPlugins: [remarkReadingTime, remarkExcerpt]
+  }
 });
---
+++ src/components/learning/PythonSliceGame.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "preact/hooks";
+import type { FunctionComponent } from "preact";
+
+export const PythonSliceGame: FunctionComponent = () => {
+    return (
+        <div>
+            
+            <form>
+                <input type="text" />
+            </form>
+        </div>
+    );
+}
