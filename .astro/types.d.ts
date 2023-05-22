8dc8d324e31cd16e37f718c3f1b5a6ca9060505d
Dylan Paulus <dylan.paulus@rover.com>
2023-05-21 19:47:17 -0700

    feat: llm article updates

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -20,8 +20,7 @@ declare module 'astro:content' {
 
 declare module 'astro:content' {
 	export { z } from 'astro/zod';
-	export type CollectionEntry<C extends keyof typeof entryMap> =
-		(typeof entryMap)[C][keyof (typeof entryMap)[C]];
+	export type CollectionEntry<C extends keyof AnyEntryMap> = AnyEntryMap[C][keyof AnyEntryMap[C]];
 
 	// TODO: Remove this when having this fallback is no longer relevant. 2.3? 3.0? - erika, 2023-04-04
 	/**
@@ -75,263 +74,357 @@ declare module 'astro:content' {
 
 	export type SchemaContext = { image: ImageFunction };
 
-	type BaseCollectionConfig<S extends BaseSchema> = {
+	type DataCollectionConfig<S extends BaseSchema> = {
+		type: 'data';
 		schema?: S | ((context: SchemaContext) => S);
 	};
-	export function defineCollection<S extends BaseSchema>(
-		input: BaseCollectionConfig<S>
-	): BaseCollectionConfig<S>;
 
-	type EntryMapKeys = keyof typeof entryMap;
+	type ContentCollectionConfig<S extends BaseSchema> = {
+		type?: 'content';
+		schema?: S | ((context: SchemaContext) => S);
+	};
+
+	type CollectionConfig<S> = ContentCollectionConfig<S> | DataCollectionConfig<S>;
+
+	export function defineCollection<S extends BaseSchema>(
+		input: CollectionConfig<S>
+	): CollectionConfig<S>;
+
 	type AllValuesOf<T> = T extends any ? T[keyof T] : never;
-	type ValidEntrySlug<C extends EntryMapKeys> = AllValuesOf<(typeof entryMap)[C]>['slug'];
+	type ValidContentEntrySlug<C extends keyof ContentEntryMap> = AllValuesOf<
+		ContentEntryMap[C]
+	>['slug'];
 
 	export function getEntryBySlug<
-		C extends keyof typeof entryMap,
-		E extends ValidEntrySlug<C> | (string & {})
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
 	>(
 		collection: C,
 		// Note that this has to accept a regular string too, for SSR
 		entrySlug: E
-	): E extends ValidEntrySlug<C>
+	): E extends ValidContentEntrySlug<C>
 		? Promise<CollectionEntry<C>>
 		: Promise<CollectionEntry<C> | undefined>;
-	export function getCollection<C extends keyof typeof entryMap, E extends CollectionEntry<C>>(
+
+	export function getDataEntryById<C extends keyof DataEntryMap, E extends keyof DataEntryMap[C]>(
+		collection: C,
+		entryId: E
+	): Promise<CollectionEntry<C>>;
+
+	export function getCollection<C extends keyof AnyEntryMap, E extends CollectionEntry<C>>(
 		collection: C,
 		filter?: (entry: CollectionEntry<C>) => entry is E
 	): Promise<E[]>;
-	export function getCollection<C extends keyof typeof entryMap>(
+	export function getCollection<C extends keyof AnyEntryMap>(
 		collection: C,
 		filter?: (entry: CollectionEntry<C>) => unknown
 	): Promise<CollectionEntry<C>[]>;
 
+	export function getEntry<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
+	>(entry: {
+		collection: C;
+		slug: E;
+	}): E extends ValidContentEntrySlug<C>
+		? Promise<CollectionEntry<C>>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof DataEntryMap,
+		E extends keyof DataEntryMap[C] | (string & {})
+	>(entry: {
+		collection: C;
+		id: E;
+	}): E extends keyof DataEntryMap[C]
+		? Promise<DataEntryMap[C][E]>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
+	>(
+		collection: C,
+		slug: E
+	): E extends ValidContentEntrySlug<C>
+		? Promise<CollectionEntry<C>>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof DataEntryMap,
+		E extends keyof DataEntryMap[C] | (string & {})
+	>(
+		collection: C,
+		id: E
+	): E extends keyof DataEntryMap[C]
+		? Promise<DataEntryMap[C][E]>
+		: Promise<CollectionEntry<C> | undefined>;
+
+	/** Resolve an array of entry references from the same collection */
+	export function getEntries<C extends keyof ContentEntryMap>(
+		entries: {
+			collection: C;
+			slug: ValidContentEntrySlug<C>;
+		}[]
+	): Promise<CollectionEntry<C>[]>;
+	export function getEntries<C extends keyof DataEntryMap>(
+		entries: {
+			collection: C;
+			id: keyof DataEntryMap[C];
+		}[]
+	): Promise<CollectionEntry<C>[]>;
+
+	export function reference<C extends keyof AnyEntryMap>(
+		collection: C
+	): import('astro/zod').ZodEffects<
+		import('astro/zod').ZodString,
+		C extends keyof ContentEntryMap
+			? {
+					collection: C;
+					slug: ValidContentEntrySlug<C>;
+			  }
+			: {
+					collection: C;
+					id: keyof DataEntryMap[C];
+			  }
+	>;
+	// Allow generic `string` to avoid excessive type errors in the config
+	// if `dev` is not running to update as you edit.
+	// Invalid collection names will be caught at build time.
+	export function reference<C extends string>(
+		collection: C
+	): import('astro/zod').ZodEffects<import('astro/zod').ZodString, never>;
+
 	type ReturnTypeOrOriginal<T> = T extends (...args: any[]) => infer R ? R : T;
-	type InferEntrySchema<C extends keyof typeof entryMap> = import('astro/zod').infer<
+	type InferEntrySchema<C extends keyof AnyEntryMap> = import('astro/zod').infer<
 		ReturnTypeOrOriginal<Required<ContentConfig['collections'][C]>['schema']>
 	>;
 
-	const entryMap: {
+	type ContentEntryMap = {
 		"post": {
 "advice-for-new-software-engineers/index.md": {
-  id: "advice-for-new-software-engineers/index.md",
-  slug: "advice-for-new-software-engineers",
-  body: string,
-  collection: "post",
+	id: "advice-for-new-software-engineers/index.md";
+  slug: "advice-for-new-software-engineers";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "albums-2010-2020-decade/index.md": {
-  id: "albums-2010-2020-decade/index.md",
-  slug: "albums-2010-2020-decade",
-  body: string,
-  collection: "post",
+	id: "albums-2010-2020-decade/index.md";
+  slug: "albums-2010-2020-decade";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "clean-code/index.md": {
-  id: "clean-code/index.md",
-  slug: "clean-code",
-  body: string,
-  collection: "post",
+	id: "clean-code/index.md";
+  slug: "clean-code";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "css-box-model/index.md": {
-  id: "css-box-model/index.md",
-  slug: "css-box-model",
-  body: string,
-  collection: "post",
+	id: "css-box-model/index.md";
+  slug: "css-box-model";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "debugging-using-python/index.mdx": {
-  id: "debugging-using-python/index.mdx",
-  slug: "debugging-using-python",
-  body: string,
-  collection: "post",
+	id: "debugging-using-python/index.mdx";
+  slug: "debugging-using-python";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "encapsulating-state-with-functional-children/index.mdx": {
-  id: "encapsulating-state-with-functional-children/index.mdx",
-  slug: "encapsulating-state-with-functional-children",
-  body: string,
-  collection: "post",
+	id: "encapsulating-state-with-functional-children/index.mdx";
+  slug: "encapsulating-state-with-functional-children";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "generic-jsx-type-argument/index.md": {
-  id: "generic-jsx-type-argument/index.md",
-  slug: "generic-jsx-type-argument",
-  body: string,
-  collection: "post",
+	id: "generic-jsx-type-argument/index.md";
+  slug: "generic-jsx-type-argument";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "git-worktree/index.mdx": {
-  id: "git-worktree/index.mdx",
-  slug: "git-worktree",
-  body: string,
-  collection: "post",
+	id: "git-worktree/index.mdx";
+  slug: "git-worktree";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "global-component-registration/index.md": {
-  id: "global-component-registration/index.md",
-  slug: "global-component-registration",
-  body: string,
-  collection: "post",
+	id: "global-component-registration/index.md";
+  slug: "global-component-registration";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "higher-order-components/index.md": {
-  id: "higher-order-components/index.md",
-  slug: "higher-order-components",
-  body: string,
-  collection: "post",
+	id: "higher-order-components/index.md";
+  slug: "higher-order-components";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "house-update-2022-2023/index.mdx": {
-  id: "house-update-2022-2023/index.mdx",
-  slug: "house-update-2022-2023",
-  body: string,
-  collection: "post",
+	id: "house-update-2022-2023/index.mdx";
+  slug: "house-update-2022-2023";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "how-fb-avoids-adblockers/index.md": {
-  id: "how-fb-avoids-adblockers/index.md",
-  slug: "how-fb-avoids-adblockers",
-  body: string,
-  collection: "post",
+	id: "how-fb-avoids-adblockers/index.md";
+  slug: "how-fb-avoids-adblockers";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "improve-your-prompts-llms/index.mdx": {
-  id: "improve-your-prompts-llms/index.mdx",
-  slug: "improve-your-prompts-llms",
-  body: string,
-  collection: "post",
+	id: "improve-your-prompts-llms/index.mdx";
+  slug: "improve-your-prompts-llms";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "injecting-react-tag-types/index.md": {
-  id: "injecting-react-tag-types/index.md",
-  slug: "injecting-react-tag-types",
-  body: string,
-  collection: "post",
+	id: "injecting-react-tag-types/index.md";
+  slug: "injecting-react-tag-types";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "intro-functional-programming/index.mdx": {
-  id: "intro-functional-programming/index.mdx",
-  slug: "intro-functional-programming",
-  body: string,
-  collection: "post",
+	id: "intro-functional-programming/index.mdx";
+  slug: "intro-functional-programming";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "intro-to-flight-js/index.md": {
-  id: "intro-to-flight-js/index.md",
-  slug: "intro-to-flight-js",
-  body: string,
-  collection: "post",
+	id: "intro-to-flight-js/index.md";
+  slug: "intro-to-flight-js";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "introducing-react-scroll-to-v2/index.md": {
-  id: "introducing-react-scroll-to-v2/index.md",
-  slug: "introducing-react-scroll-to-v2",
-  body: string,
-  collection: "post",
+	id: "introducing-react-scroll-to-v2/index.md";
+  slug: "introducing-react-scroll-to-v2";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "level-terminal-navigation/index.md": {
-  id: "level-terminal-navigation/index.md",
-  slug: "level-terminal-navigation",
-  body: string,
-  collection: "post",
+	id: "level-terminal-navigation/index.md";
+  slug: "level-terminal-navigation";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "lost-connections/index.md": {
-  id: "lost-connections/index.md",
-  slug: "lost-connections",
-  body: string,
-  collection: "post",
+	id: "lost-connections/index.md";
+  slug: "lost-connections";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "making-react-fast/index.md": {
-  id: "making-react-fast/index.md",
-  slug: "making-react-fast",
-  body: string,
-  collection: "post",
+	id: "making-react-fast/index.md";
+  slug: "making-react-fast";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "node-dependency-types/index.md": {
-  id: "node-dependency-types/index.md",
-  slug: "node-dependency-types",
-  body: string,
-  collection: "post",
+	id: "node-dependency-types/index.md";
+  slug: "node-dependency-types";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "not-everything-is-state/index.md": {
-  id: "not-everything-is-state/index.md",
-  slug: "not-everything-is-state",
-  body: string,
-  collection: "post",
+	id: "not-everything-is-state/index.md";
+  slug: "not-everything-is-state";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "postgres-is-a-graph-database/index.mdx": {
-  id: "postgres-is-a-graph-database/index.mdx",
-  slug: "postgres-is-a-graph-database",
-  body: string,
-  collection: "post",
+	id: "postgres-is-a-graph-database/index.mdx";
+  slug: "postgres-is-a-graph-database";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".mdx"] },
+} & { render(): Render[".mdx"] };
 "react-controlling-rendering-with-keys/index.md": {
-  id: "react-controlling-rendering-with-keys/index.md",
-  slug: "react-controlling-rendering-with-keys",
-  body: string,
-  collection: "post",
+	id: "react-controlling-rendering-with-keys/index.md";
+  slug: "react-controlling-rendering-with-keys";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "reusable-components-factory-component/index.md": {
-  id: "reusable-components-factory-component/index.md",
-  slug: "reusable-components-factory-component",
-  body: string,
-  collection: "post",
+	id: "reusable-components-factory-component/index.md";
+  slug: "reusable-components-factory-component";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "start-with-no/index.md": {
-  id: "start-with-no/index.md",
-  slug: "start-with-no",
-  body: string,
-  collection: "post",
+	id: "start-with-no/index.md";
+  slug: "start-with-no";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "tips-on-creating-reusable-components/index.md": {
-  id: "tips-on-creating-reusable-components/index.md",
-  slug: "tips-on-creating-reusable-components",
-  body: string,
-  collection: "post",
+	id: "tips-on-creating-reusable-components/index.md";
+  slug: "tips-on-creating-reusable-components";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "tips-on-improving-fabricjs-speed/index.md": {
-  id: "tips-on-improving-fabricjs-speed/index.md",
-  slug: "tips-on-improving-fabricjs-speed",
-  body: string,
-  collection: "post",
+	id: "tips-on-improving-fabricjs-speed/index.md";
+  slug: "tips-on-improving-fabricjs-speed";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "using-typescript-with-react-part-one/index.md": {
-  id: "using-typescript-with-react-part-one/index.md",
-  slug: "using-typescript-with-react-part-one",
-  body: string,
-  collection: "post",
+	id: "using-typescript-with-react-part-one/index.md";
+  slug: "using-typescript-with-react-part-one";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "using-typescript-with-redux-part-two/index.md": {
-  id: "using-typescript-with-redux-part-two/index.md",
-  slug: "using-typescript-with-redux-part-two",
-  body: string,
-  collection: "post",
+	id: "using-typescript-with-redux-part-two/index.md";
+  slug: "using-typescript-with-redux-part-two";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
+} & { render(): Render[".md"] };
 "write-software-that-evolves/index.md": {
-  id: "write-software-that-evolves/index.md",
-  slug: "write-software-that-evolves",
-  body: string,
-  collection: "post",
+	id: "write-software-that-evolves/index.md";
+  slug: "write-software-that-evolves";
+  body: string;
+  collection: "post";
   data: InferEntrySchema<"post">
-} & { render(): Render[".md"] },
-},
+} & { render(): Render[".md"] };
+};
 
 	};
+
+	type DataEntryMap = {
+		
+	};
+
+	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
 	type ContentConfig = typeof import("../src/content/config");
 }
