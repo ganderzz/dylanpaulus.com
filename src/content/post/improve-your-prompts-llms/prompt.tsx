07987d24f092c81ae64fe8129342131d7ffe8c0d
Dylan Paulus <dylan.paulus@rover.com>
2023-06-06 21:50:37 -0700

    prompt engineering

--- src/content/post/improve-your-prompts-llms/prompt.tsx
+++ src/content/post/improve-your-prompts-llms/prompt.tsx
@@ -1,13 +1,28 @@
 export const Prompt = ({
   children,
   header,
+  isResponse,
+  className = "",
+  ...rest
 }: {
   children: any;
   header: string;
+  className?: string;
+  isResponse?: boolean;
 }) => {
+  const color = isResponse
+    ? `border-green-600 dark:border-green-600 bg-green-200 dark:bg-green-700`
+    : `border-slate-200 dark:border-slate-600 bg-gray-100 dark:bg-slate-800`;
+
+  const headerColor = isResponse
+    ? `bg-green-500 dark:bg-green-800`
+    : `bg-gray-200 dark:bg-slate-900`;
+
   return (
-    <div class="bg-slate-800 border-solid border-2 border-slate-500 rounded-lg my-4">
-      <div class="text-base bg-slate-900 rounded px-4 py-2 font-semibold">
+    <div
+      class={`border-solid border-2 ${color} rounded-lg my-4 ${className}`}
+      {...rest}>
+      <div class={`text-base ${headerColor} rounded px-4 py-2 font-semibold`}>
         {header}
       </div>
 
