9651fd3df84826f0f92da89ad000ef677a4a9eb8
Dylan Paulus <dylantpaulus@gmail.com>
2024-11-10 20:17:23 -0800

    add new articles & update rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,5 +1,12 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt({
+  html: true,
+  breaks: true,
+});
 
 export const GET = async () => {
   const posts = await getCollection("post");
@@ -15,6 +22,9 @@ export const GET = async () => {
       link: `/posts/${post.slug}`,
       title: post.data.title,
       pubDate: post.data.date as any,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
     })),
     customData: `<language>en-us</language>`,
   });
