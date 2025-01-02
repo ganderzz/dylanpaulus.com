3741bb239096aad8564a3d4e63654d47af991594
Dylan Paulus <dylantpaulus@gmail.com>
2025-01-01 22:57:06 -0800

    tags

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -141,315 +141,315 @@ declare module 'astro:content' {
   slug: "advice-for-new-software-engineers";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "albums-2010-2020-decade/index.md": {
 	id: "albums-2010-2020-decade/index.md";
   slug: "albums-2010-2020-decade";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "clean-code/index.md": {
 	id: "clean-code/index.md";
   slug: "clean-code";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "common-table-expressions-in-sql/index.mdx": {
 	id: "common-table-expressions-in-sql/index.mdx";
   slug: "common-table-expressions-in-sql";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "creating-fast-time-series-graph-with-postgres/index.mdx": {
 	id: "creating-fast-time-series-graph-with-postgres/index.mdx";
   slug: "creating-fast-time-series-graph-with-postgres";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "css-box-model/index.md": {
 	id: "css-box-model/index.md";
   slug: "css-box-model";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "debugging-using-python/index.mdx": {
 	id: "debugging-using-python/index.mdx";
   slug: "debugging-using-python";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "easily-generate-mock-data-with-postgresql/index.mdx": {
 	id: "easily-generate-mock-data-with-postgresql/index.mdx";
   slug: "easily-generate-mock-data-with-postgresql";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "encapsulating-state-with-functional-children/index.mdx": {
 	id: "encapsulating-state-with-functional-children/index.mdx";
   slug: "encapsulating-state-with-functional-children";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "explaining-postgresql-explain/index.mdx": {
 	id: "explaining-postgresql-explain/index.mdx";
   slug: "explaining-postgresql-explain";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "exploring-adjacency-lists-and-matrices/index.mdx": {
 	id: "exploring-adjacency-lists-and-matrices/index.mdx";
   slug: "exploring-adjacency-lists-and-matrices";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "generic-jsx-type-argument/index.md": {
 	id: "generic-jsx-type-argument/index.md";
   slug: "generic-jsx-type-argument";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "git-worktree/index.mdx": {
 	id: "git-worktree/index.mdx";
   slug: "git-worktree";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "global-component-registration/index.md": {
 	id: "global-component-registration/index.md";
   slug: "global-component-registration";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "higher-order-components/index.md": {
 	id: "higher-order-components/index.md";
   slug: "higher-order-components";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "how-fb-avoids-adblockers/index.md": {
 	id: "how-fb-avoids-adblockers/index.md";
   slug: "how-fb-avoids-adblockers";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "how-to-design-postgres-database-schemas/index.mdx": {
 	id: "how-to-design-postgres-database-schemas/index.mdx";
   slug: "how-to-design-postgres-database-schemas";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "how-to-reduce-bloat-in-large-postgresql-tables/index.mdx": {
 	id: "how-to-reduce-bloat-in-large-postgresql-tables/index.mdx";
   slug: "how-to-reduce-bloat-in-large-postgresql-tables";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "how-to-reduce-your-postgresql-database-size/index.mdx": {
 	id: "how-to-reduce-your-postgresql-database-size/index.mdx";
   slug: "how-to-reduce-your-postgresql-database-size";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "how-to-use-postgres-for-data-normalization/index.mdx": {
 	id: "how-to-use-postgres-for-data-normalization/index.mdx";
   slug: "how-to-use-postgres-for-data-normalization";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "improve-your-prompts-llms/index.mdx": {
 	id: "improve-your-prompts-llms/index.mdx";
   slug: "improve-your-prompts-llms";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "injecting-react-tag-types/index.md": {
 	id: "injecting-react-tag-types/index.md";
   slug: "injecting-react-tag-types";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "intro-functional-programming/index.mdx": {
 	id: "intro-functional-programming/index.mdx";
   slug: "intro-functional-programming";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "intro-to-flight-js/index.md": {
 	id: "intro-to-flight-js/index.md";
   slug: "intro-to-flight-js";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "introducing-react-scroll-to-v2/index.md": {
 	id: "introducing-react-scroll-to-v2/index.md";
   slug: "introducing-react-scroll-to-v2";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "level-terminal-navigation/index.md": {
 	id: "level-terminal-navigation/index.md";
   slug: "level-terminal-navigation";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "lost-connections/index.md": {
 	id: "lost-connections/index.md";
   slug: "lost-connections";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "making-react-fast/index.mdx": {
 	id: "making-react-fast/index.mdx";
   slug: "making-react-fast";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "node-dependency-types/index.md": {
 	id: "node-dependency-types/index.md";
   slug: "node-dependency-types";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "not-everything-is-state/index.md": {
 	id: "not-everything-is-state/index.md";
   slug: "not-everything-is-state";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "postgres-is-a-graph-database/index.mdx": {
 	id: "postgres-is-a-graph-database/index.mdx";
   slug: "postgres-is-a-graph-database";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "postgres-security-guide/index.mdx": {
 	id: "postgres-security-guide/index.mdx";
   slug: "postgres-security-guide";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "react-controlling-rendering-with-keys/index.md": {
 	id: "react-controlling-rendering-with-keys/index.md";
   slug: "react-controlling-rendering-with-keys";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "recursive-query-in-sql-what-it-is-and-how-to-write-one/index.mdx": {
 	id: "recursive-query-in-sql-what-it-is-and-how-to-write-one/index.mdx";
   slug: "recursive-query-in-sql-what-it-is-and-how-to-write-one";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "resizing-mac-aws-volumes/index.mdx": {
 	id: "resizing-mac-aws-volumes/index.mdx";
   slug: "resizing-mac-aws-volumes";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "reusable-components-factory-component/index.md": {
 	id: "reusable-components-factory-component/index.md";
   slug: "reusable-components-factory-component";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "start-with-no/index.md": {
 	id: "start-with-no/index.md";
   slug: "start-with-no";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "tips-on-creating-reusable-components/index.md": {
 	id: "tips-on-creating-reusable-components/index.md";
   slug: "tips-on-creating-reusable-components";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "tips-on-improving-fabricjs-speed/index.md": {
 	id: "tips-on-improving-fabricjs-speed/index.md";
   slug: "tips-on-improving-fabricjs-speed";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "understanding-postgres-foreign-keys/index.mdx": {
 	id: "understanding-postgres-foreign-keys/index.mdx";
   slug: "understanding-postgres-foreign-keys";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "understanding-postgres-tablespaces/index.mdx": {
 	id: "understanding-postgres-tablespaces/index.mdx";
   slug: "understanding-postgres-tablespaces";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "understanding-sql-aggregate-functions/index.mdx": {
 	id: "understanding-sql-aggregate-functions/index.mdx";
   slug: "understanding-sql-aggregate-functions";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".mdx"] };
 "using-typescript-with-react-part-one/index.md": {
 	id: "using-typescript-with-react-part-one/index.md";
   slug: "using-typescript-with-react-part-one";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "using-typescript-with-redux-part-two/index.md": {
 	id: "using-typescript-with-redux-part-two/index.md";
   slug: "using-typescript-with-redux-part-two";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 "write-software-that-evolves/index.md": {
 	id: "write-software-that-evolves/index.md";
   slug: "write-software-that-evolves";
   body: string;
   collection: "post";
-  data: any
+  data: InferEntrySchema<"post">
 } & { render(): Render[".md"] };
 };
 
@@ -461,5 +461,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = never;
+	export type ContentConfig = typeof import("../src/content/config.js");
 }
